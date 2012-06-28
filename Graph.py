f7df926e1e087719d4c62626c2a6f1258d21f547
Mihnea Dobrescu-Balaur <mihneadb@gmail.com>
2012-06-28 15:06:17 +0300

    remove_edge

--- Graph.py
+++ Graph.py
@@ -34,6 +34,17 @@ class Graph(dict):
                 return edges[v]
         return None
 
+    def remove_edge(self, e):
+        """ remove all references of e from the graph
+        """
+        # obtain the vertices
+        u, v = e
+        if u in self:
+            edges = self[u]
+            if v in edges:
+                del edges[v]
+                del self[v][u]
+
 class Vertex(object):
     def __init__(self, label=''):
         self.label = label
@@ -51,6 +62,9 @@ class Edge(tuple):
     def __repr__(self):
         return 'Edge(%s, %s)' % (repr(self[0]), repr(self[1]))
 
+    def vertices(self):
+        return (self[0], self[1])
+
     __str__ = __repr__
 
 
@@ -64,3 +78,5 @@ if __name__ == '__main__':
     print g
     print g.get_edge(w, v)
     print g.get_edge(w, Vertex('lulu'))
+    g.remove_edge(e)
+    print g
